1865c3f38040368ff0263690f3451342cfee22aa
gabrielrowan <gabrielrowan82@gmail.com>
2024-11-20 16:47:53 +0000

    there is a bug in this code!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//If I click on the add button, the cart total increases by 1
+//If I click on the increae quantity button it adds 1 to the quantity
 
 
 // Checks if DOM is ready before executing javascript referencing DOM elements
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function ()
     addAddButtonFunctionality();
 
 });
+
+
 
 // Adds on click functionality to add Buttons
 const addAddButtonFunctionality = () =>
@@ -19,6 +21,23 @@ const addAddButtonFunctionality = () =>
     }
 }
 
+const increaseQuantity = (buttonParent, event) =>
+{
+    const quantityInput = buttonParent.querySelector(".input-cart-quantity");
+    console.log(quantityInput);
+    const currentQuantity = parseInt(quantityInput.value);
+    quantityInput.value = currentQuantity + 1;
+
+}
+
+// Adds functionality to increase quantity button
+const addIncreaseQuantityButtonFunctionality = (buttonParent, event) =>
+{
+    const increaseQuantityButton = buttonParent.querySelector(".increase-quantity");
+    increaseQuantityButton.addEventListener("click", increaseQuantity(buttonParent, event));
+
+}
+
 
 // Removes add button from product card and replaces it with quantity input controls
 // Increases cart count by 1
@@ -29,6 +48,7 @@ const addButtonClicked = event =>
     addButton.remove();
     addCartItemInput(buttonParent);
     addItemToCart();
+    addIncreaseQuantityButtonFunctionality(buttonParent, event);
 }
 
 //Increases total cart item count by 1
@@ -44,11 +64,13 @@ const addItemToCart = () =>
 const addCartItemInput = parentElement =>
 {
     const increaseQuantityButton = document.createElement("button");
-    increaseQuantityButton.className = "button-cart-quantity";
+    const increaseQuantityButtonClassList = ["button-cart-quantity", "increase-quantity"]
+    increaseQuantityButton.classList.add(...increaseQuantityButtonClassList);
     increaseQuantityButton.innerText = "+";
 
     const decreaseQuantityButton = document.createElement("button");
-    decreaseQuantityButton.className = "button-cart-quantity";
+    const decreaseQuantityButtonClassList = ["button-cart-quantity", "decrease-quantity"]
+    decreaseQuantityButton.classList.add(...decreaseQuantityButtonClassList);
     decreaseQuantityButton.innerText = "-";
 
     const quantityInputDiv = document.createElement("div");
